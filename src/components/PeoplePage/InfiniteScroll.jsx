87eb295e0db4f5c1c80dd9400524a90ccb4127b2
anonymousrecords <a.Records2022@gmail.com>
2023-07-16 22:52:56 +0900

    feat : Implement Infinite Scroll

---
+++ src/components/PeoplePage/InfiniteScroll.jsx
@@ -0,0 +1,56 @@
+import { useRef, useCallback } from 'react'
+import Post from './Post'
+import { useInfiniteQuery } from 'react-query'
+import { getPostsPage } from '../../api/axios'
+
+const InfiniteScroll = () => {
+
+    const {
+        fetchNextPage, //function 
+        hasNextPage, // boolean
+        isFetchingNextPage, // boolean
+        data,
+        status,
+        error
+    } = useInfiniteQuery('/posts', ({ pageParam = 1 }) => getPostsPage(pageParam), {
+        getNextPageParam: (lastPage, allPages) => {
+            return lastPage.length ? allPages.length + 1 : undefined
+        }
+    })
+
+    const intObserver = useRef()
+    const lastPostRef = useCallback(post => {
+        if (isFetchingNextPage) return
+
+        if (intObserver.current) intObserver.current.disconnect()
+
+        intObserver.current = new IntersectionObserver(posts => {
+            if (posts[0].isIntersecting && hasNextPage) {
+                console.log('We are near the last post!')
+                fetchNextPage()
+            }
+        })
+
+        if (post) intObserver.current.observe(post)
+    }, [isFetchingNextPage, fetchNextPage, hasNextPage])
+
+    if (status === 'error') return <p className='center'>Error: {error.message}</p>
+
+    const content = data?.pages.map(pg => {
+        return pg.map((post, i) => {
+            if (pg.length === i + 1) {
+                return <Post ref={lastPostRef} key={post.id} post={post} />
+            }
+            return <Post key={post.id} post={post} />
+        })
+    })
+
+    return (
+        <>
+            {content}
+            {isFetchingNextPage && <p className="center">Loading...</p>}
+            <p className="center"><a href="#top">위로 올라가기</a></p>
+        </>
+    )
+}
+export default InfiniteScroll
