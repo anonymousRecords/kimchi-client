f031a157942302f673ec34c41f6b390bd72f250f
anonymousrecords <a.Records2022@gmail.com>
2023-07-24 03:13:05 +0900

    feat : Implement DecoratePage header

--- src/pages/DecoratePage.jsx
+++ src/pages/DecoratePage.jsx
@@ -1,4 +1,6 @@
+import React, { useState } from "react"; // eslint-disable-line no-unused-vars
 import styled from 'styled-components';
+import { Link } from "react-router-dom";
 
 const StyledDecorate = styled.div`
     width: 390px;
@@ -7,14 +9,104 @@ const StyledDecorate = styled.div`
     flex-direction: column;
     align-items: center;
     background-color: #F7F7F7;
+
+    header {
+        width: 390px;
+        height: 73px;
+        padding: 16px;
+        background-color: red;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .decorate-box {
+        width: 358px;
+        height: 48px;
+        border-radius: 8px;
+        background-color: #EEEEEE;
+        padding: 4px;
+        display: flex;
+        gap: 8px;
+    }
+
+    .frame {
+        width: 172px;
+        height: 40px;
+        border-radius: 8px;
+        // background-color: black;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .photo {
+        width: 172px;
+        height: 40px;
+        border-radius: 8px;
+        // background-color: black;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .btn-container{
+        width: 118px;
+        height: 18px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 8px;
+    }
+
+    .btn-container > p {
+        color: white;
+    }
+
+    .selected {
+        background-color: black;
+        color: white; /* (Optional) Change text color when selected */
+      }
 `
 
 const DecoratePage = () => {
+    const [selectedButton, setSelectedButton] = useState("frame");
+  
     return (
-        <StyledDecorate>
-            <div>Decorate Page</div>
-        </StyledDecorate>
+      <StyledDecorate>
+        <header>
+          <button>
+            <Link to="/make">
+              <img src='/assets/icons/arrow-back.svg' alt='back-space'/>
+            </Link>
+          </button>
+          <button>Done</button>
+        </header>
+        <div className='decorate-container'>
+          <div className='decorate-box'>
+            <button
+              className={`frame ${selectedButton === "frame" ? "selected" : ""}`}
+              onClick={() => setSelectedButton("frame")}
+            >
+              <div className='btn-container'>
+                <img src='/assets/icons/palette.svg' alt='palette'/>
+                <div>Frame Design</div>
+              </div>
+            </button>
+            <button
+              className={`photo ${selectedButton === "photo" ? "selected" : ""}`}
+              onClick={() => setSelectedButton("photo")}
+            >
+              <div className='btn-container'>
+                <img src='/assets/icons/ai-image.svg'alt='ai-img'/>
+                <div>Photo AI</div>
+              </div>
+            </button>
+          </div>
+        </div>
+      </StyledDecorate>
     )
-};
-
-export default DecoratePage;
+  };
+  
+  export default DecoratePage;
