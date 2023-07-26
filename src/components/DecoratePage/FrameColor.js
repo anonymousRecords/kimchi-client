0d2c382013b34140e0f1952a582bd495961c44fd
anonymousrecords <a.Records2022@gmail.com>
2023-07-26 20:26:58 +0900

    feat : Imlement FrameTool

---
+++ src/components/DecoratePage/FrameColor.js
@@ -0,0 +1,27 @@
+export const FrameColor = [
+    {
+        color: 'pink',
+        name : 'AI',
+    },
+    {
+        color: 'black',
+        name : 'black',
+    },
+    {
+        color: 'white',
+        name : 'white',
+    },
+    {
+        color: 'grey',
+        name : 'grey',
+    },
+    {
+        color: 'purple',
+        name : 'purple',
+    },
+    {
+        color: 'skyblue',
+        name : 'skyblue',
+    },
+
+];
--- src/components/DecoratePage/FrameTool.jsx
+++ src/components/DecoratePage/FrameTool.jsx
@@ -1,13 +1,45 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
 import styled from 'styled-components';
+import { FrameColor } from './FrameColor';
 
 const StyledFrameTool = styled.div`
+  width: 420px;
+  height: 100px;
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
+  padding-left: 16px;
 
-`
+  .frame-choice {
+    width: 60px;
+    height: 77px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
-export default function FrameTool () {
-    return(
-        <StyledFrameTool>
-            <div>프레임 꾸미기</div>
-        </StyledFrameTool>
-    )
+  .color-box {
+    width: 60px;
+    height: 60px;
+    border-radius: 8px;
+    margin-bottom: 2px;
+  }
+
+  .color-name {
+    font-size: 12px;
+  }
+`;
+
+export default function FrameTool() {
+  return (
+    <StyledFrameTool>
+      {FrameColor.map((frame) => (
+        <div className="frame-choice" key={frame.color}>
+          <div className="color-box" style={{ backgroundColor: frame.color }}></div>
+          <div className="color-name">{frame.name}</div>
+        </div>
+      ))}
+    </StyledFrameTool>
+  );
 }
