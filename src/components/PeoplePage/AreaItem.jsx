7ecee5d9f039d887033392310e2f777883785784
anonymousrecords <a.Records2022@gmail.com>
2023-07-26 20:01:59 +0900

    fix : Add PropTypes

--- src/components/PeoplePage/AreaItem.jsx
+++ src/components/PeoplePage/AreaItem.jsx
@@ -1,57 +1,65 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const StyledAreaItem = styled.div`
-font-size: 36px;
-font-weight: 800;
+  font-size: 36px;
+  font-weight: 800;
 
-.toggle-container {
-  width: 128px;
-  height: 340px;
-  padding: 12px;
-  border-radius: 8px;
-  font-size: 16px;
-  font-weight: 400;
-  color: #A9A9A9;
-  background-color: white;
-  cursor: pointer;
-}
-.toggle-container > div:hover {
-  color: black;
-  font-weight: 600;
-}
+  .toggle-container {
+    width: 128px;
+    height: 340px;
+    padding: 12px;
+    border-radius: 8px;
+    font-size: 16px;
+    font-weight: 400;
+    color: #A9A9A9;
+    background-color: white;
+    cursor: pointer;
+  }
+
+  .toggle-container > div:hover {
+    color: black;
+    font-weight: 600;
+  }
 `
 
-export default function AreaItem ({ title, content, onContentClick }) {
-    const [isExpanded, setIsExpanded] = useState(false);
-  
-    const handleToggle = () => {
-      setIsExpanded(!isExpanded);
-    };
-  
-    const handleContentClick = (clickedTitle) => {
-      onContentClick(clickedTitle);
-      setIsExpanded(false);
-    };
-  
-    return (
-      <StyledAreaItem>
-        <button onClick={handleToggle}>
-          {isExpanded ? title : title}
-          <img src='/assets/icons/toggle.svg' alt='toggle'/>
-        </button>
-        {isExpanded && (
-          <div className='toggle-container'>
-            {content.map((item, index) => (
-              <div
-                key={index}
-                onClick={() => handleContentClick(item)}
-              >
-                {item}
-              </div>
-            ))}
-          </div>
-        )}
-      </StyledAreaItem>
-    );
+export default function AreaItem({ title, content, onContentClick }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const handleContentClick = (clickedTitle) => {
+    onContentClick(clickedTitle);
+    setIsExpanded(false);
+  };
+
+  return (
+    <StyledAreaItem>
+      <button onClick={handleToggle}>
+        {isExpanded ? title : title}
+        <img src='/assets/icons/toggle.svg' alt='toggle' />
+      </button>
+      {isExpanded && (
+        <div className='toggle-container'>
+          {content.map((item, index) => (
+            <div
+              key={index}
+              onClick={() => handleContentClick(item)}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      )}
+    </StyledAreaItem>
+  );
+}
+
+AreaItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onContentClick: PropTypes.func.isRequired,
 };
