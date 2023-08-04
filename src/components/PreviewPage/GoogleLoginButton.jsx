9e4436ac63ae58ae91553e51c443a72284a82687
anonymousrecords <a.Records2022@gmail.com>
2023-08-04 19:41:09 +0900

    feat : Implement GoogleLogin

--- src/components/PreviewPage/GoogleLoginButton.jsx
+++ src/components/PreviewPage/GoogleLoginButton.jsx
@@ -1,14 +1,17 @@
 import {GoogleLogin} from "@react-oauth/google";
 import {GoogleOAuthProvider} from "@react-oauth/google";
+import { useNavigate } from 'react-router-dom';
 
 const GoogleLoginButton = () => {
-    const clientId = 'clientID'
+    const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
+    const navigate = useNavigate();
     return (
         <>
             <GoogleOAuthProvider clientId={clientId}>
                 <GoogleLogin
                     onSuccess={(res) => {
                         console.log(res);
+                        navigate('/boast', { state: {} });
                     }}
                     onFailure={(err) => {
                         console.log(err);
