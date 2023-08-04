9ac274a41bc0e0a10521e3207378f1dbf9c16560
anonymousrecords <a.Records2022@gmail.com>
2023-08-05 07:52:03 +0900

    feat : Impelement BoastCompeleteCard

---
+++ src/components/BoastCompeletePage/BoastCompeleteCard.jsx
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import { useRecoilValue } from "recoil";
+import { CardImageAtom } from "../../recoil/CardImageAtom";
+import { FrameColorAtom } from "../../recoil/FrameColorAtom";
+
+const CompeleteCardContainer = styled.div`
+    box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.10);
+`
+const CardHeader = styled.div`
+    width: 358px;
+    height: 30px;
+    border-radius: 8px 8px 0 0;
+    background-color: white;
+    padding: 7px 0 0 12px;
+    border: solid 1px #d8d8d8;
+    border-bottom: none;
+`
+const UserID = styled.div`
+    font-size: 12px;
+    font-weight: 600;
+`
+
+const ImageFrame = styled.div`
+  background-color: ${(props) => props.frameColor};
+  display: grid;
+  grid-gap: 6px;
+  grid-template-columns: repeat(2, 1fr);
+  width: 358px;
+  height: 409px;
+  padding: 6px;
+  border-left: solid 1px #d8d8d8;
+  border-right: solid 1px #d8d8d8;
+`
+
+const StyledImageWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  &:before {
+    display: block;
+    content: "";
+    padding-bottom: 100%;
+  }
+`;
+
+const StyledImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const CardFooter = styled.div`
+    width: 358px;
+    height: 51px;
+    border-radius: 0 0 8px 8px;
+    background-color: white;
+    padding: 10px 0 0 12px;
+    border: solid 1px #d8d8d8;
+    border-top: none;
+`
+
+const LocationName = styled.div`
+    font-size: 12px;
+    font-weight: 600;
+`
+
+const LocationContent = styled.div`
+    font-size: 12px;
+`
+
+export default function BoastCompeleteCard() {
+  const imageList = useRecoilValue(CardImageAtom);
+  const frameColorChoice = useRecoilValue(FrameColorAtom);
+
+  return (
+    <CompeleteCardContainer>
+        <CardHeader>
+            <UserID>@NEON</UserID>
+        </CardHeader>
+        <ImageFrame frameColor={frameColorChoice.color}>
+        {imageList.map((imageBlob, index) => (
+            <StyledImageWrapper key={index}>
+            <StyledImage src={URL.createObjectURL(imageBlob)} />
+            </StyledImageWrapper>
+        ))}
+        </ImageFrame>
+        <CardFooter>
+            <LocationName>가천대학교 글로벌캠퍼스</LocationName>
+            <LocationContent>경기도 성남시 수정구 성남대로 1342</LocationContent>
+        </CardFooter>
+    </CompeleteCardContainer>
+  );
+}
--- src/pages/BoastCompeletePage.jsx
+++ src/pages/BoastCompeletePage.jsx
@@ -1,18 +1,75 @@
-import styled from 'styled-components';
-import BoastCompeleteHeader from '../components/BoastCompeletePage/BoastComepleteHeader';
+import styled from "styled-components";
+import BoastCompeleteHeader from "../components/BoastCompeletePage/BoastComepleteHeader";
+import BoastCompeleteCard from "../components/BoastCompeletePage/BoastCompeleteCard";
+import { useNavigate } from "react-router-dom";
 
 const StyledBoastComeplete = styled.div`
-    width: 390px;
-    background-color: #F7F7F7;
-`
+  width: 390px;
+  background-color: #f7f7f7;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  padding: 16px;
+  gap: 8px;
+  margin-top: 24px;
+`;
+
+const ResetLocationButton = styled.button`
+  width: 176px;
+  height: 52px;
+  border-radius: 8px;
+  border: 0;
+  background-color: white;
+  border: 1px solid black;
+  cursor: pointer;
+  font-size: 18px;
+  color: black;
+  font-weight: 600;
+`;
+
+const BoastButton = styled.button`
+  width: 176px;
+  height: 52px;
+  border-radius: 8px;
+  border: 0;
+  background-color: black;
+  cursor: pointer;
+  font-size: 18px;
+  color: white;
+  font-weight: 600;
+`;
 
 const BoastComepletePage = () => {
-    return (
-        <StyledBoastComeplete>
-            <BoastCompeleteHeader/>
-
-        </StyledBoastComeplete>
-    );
-}
+  const navigate = useNavigate();
+  return (
+    <StyledBoastComeplete>
+      <BoastCompeleteHeader />
+      <BoastCompeleteCard />
+      <ButtonContainer>
+        <ResetLocationButton
+          onClick={() => {
+            navigate("/search");
+          }}
+        >
+          위치 재설정하기
+        </ResetLocationButton>
+        <BoastButton
+          onClick={() => {
+            navigate("/people");
+          }}
+        >
+          자랑하기
+        </BoastButton>
+      </ButtonContainer>
+    </StyledBoastComeplete>
+  );
+};
 
 export default BoastComepletePage;
