3be6ff5257a74e0222b842d6012d92911d4839f8
anonymousrecords <a.Records2022@gmail.com>
2023-08-04 19:44:17 +0900

    feat : Impelement MyPage

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,18 +1,34 @@
-import styled from 'styled-components';
-import BottomNav from '../components/BottomNav.jsx';
+import styled from "styled-components";
+import GoogleLoginButton from "../components/PreviewPage/GoogleLoginButton";
+import BottomNav from "../components/BottomNav.jsx";
 
 const StyledMy = styled.div`
-    width: 390px;
-    background-color: #F7F7F7;
 `
 
-const MyPage = () => {
-    return (
-        <StyledMy>
-            <div>MyPage</div>
-            <BottomNav type="my"/>
-        </StyledMy>
-    );
+const Header = styled.div`
+    font-size: 32px;
+    font-weight: 600;
+    height: 124px;
+    padding-left: 24px;
+    padding-top: 60px;
+`
+
+const MyPageContent = styled.div`
+    height: 638px;
+    display: flex;
+    justify-content: center;
+`
+
+function MyPage() {
+  return (
+    <StyledMy>
+        <Header>mypage</Header>
+        <MyPageContent>
+            <GoogleLoginButton/>
+        </MyPageContent>
+        <BottomNav type="my" />
+    </StyledMy>
+  );
 }
 
 export default MyPage;
