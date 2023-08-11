c64ab75e25472554bcc5bd5f49e503c5670e2b4d
anonymousrecords <a.Records2022@gmail.com>
2023-08-12 01:31:17 +0900

    chore : 코드 임시 주석처리

--- src/components/MakePage/ImageCard.jsx
+++ src/components/MakePage/ImageCard.jsx
@@ -1,192 +1,277 @@
-import { useEffect } from 'react';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
+// import { useEffect } from 'react';
+// import styled from 'styled-components';
+// import PropTypes from 'prop-types';
+// import { useRecoilState } from 'recoil';
+// import { CardImageAtom } from '../../recoil/CardImageAtom';
 
-const StyledImageCard = styled.div`
-.img-frame {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    // margin-top: 8.813rem;
-    width: 358px;
-    height: var(--image-frame-height);
-    padding: 6px;
-    padding-bottom: 57px;
-    background: #E6E6E6;
-    transform: translateY(45px);
-    border: solid 1px #D8D8D8;
-}
+// const StyledImageCard = styled.div`
+//   margin-top: 140px;
 
-.img-row-container{
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-}
+//   .img-frame {
+//       display: flex;
+//       flex-direction: column;
+//       align-items: center;
+//       // margin-top: 8.813rem;
+//       width: 358px;
+//       height: var(--image-frame-height);
+//       padding: 6px;
+//       padding-bottom: 57px;
+//       background: #E6E6E6;
+//       transform: translateY(45px);
+//       border: solid 1px #D8D8D8;
+//   }
 
-.img-select {
-    position: relative;
-    width: 164px;
-    height: 164px;
-    background: #fff;
-    overflow: hidden;
-    margin: 6px;
-}
+//   .img-row-container{
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//   }
 
-.img-select.empty {
-    background-image: url("/assets/icons/add-picture.svg");
-    background-position: center;
-    background-repeat: no-repeat;
-}
+//   .img-select {
+//       position: relative;
+//       width: 164px;
+//       height: 164px;
+//       background: #fff;
+//       overflow: hidden;
+//       margin: 6px;
+//   }
 
-.img-select.empty:hover {
-    background-color: rgb(247, 247, 247);
-}
+//   .img-select.empty {
+//       background-image: url("/assets/icons/add-picture.svg");
+//       background-position: center;
+//       background-repeat: no-repeat;
+//   }
 
-.img {
-    position: absolute;
-    width: 236px;
-    height: auto;
-    -webkit-user-drag: none;
-    -khtml-user-drag: none;
-    -moz-user-drag: none;
-    -o-user-drag: none;
-    user-drag: none;
-    object-fit: cover;
-}
-`
-ImageCard.propTypes = {
-  onImagesSelected: PropTypes.func,
-};
+//   .img-select.empty:hover {
+//       background-color: rgb(247, 247, 247);
+//   }
 
-export default function ImageCard ({ onImagesSelected }) {
-  // 이미지 파일인지 확인
-  const fileExtentionCheck = (event) => {
-    const file = event.target;
-    const fileName = file.files[0].name.toLowerCase();
+//   .img {
+//       position: absolute;
+//       width: 236px;
+//       height: auto;
+//       -webkit-user-drag: none;
+//       -khtml-user-drag: none;
+//       -moz-user-drag: none;
+//       -o-user-drag: none;
+//       user-drag: none;
+//       object-fit: cover;
+//   }
+// `
+// const ImageRow = styled.div`
+//   display: flex;
+// `
 
-    if (fileName.endsWith('jpg') || fileName.endsWith('png') || fileName.endsWith('jpeg')){
-      return true;
-    }
+// ImageCard.propTypes = {
+//   data: PropTypes.func,
+//   onImagesSelected: PropTypes.func,
+// };
 
-    alert('png, jpg, jpeg 파일만 등록 가능합니다.');
-    file.value = '';
-    return false;
-  };
+// export default function ImageCard ({data, onImagesSelected}) {
+//   const [imageList, setImageList] = useRecoilState(CardImageAtom);
 
-  // 사진 업로드
-  const imageUpload = (e) => {
-    if (e.target.className.includes('drag')) {
-      e.target.classList.remove('drag');
-      return;
-    }
+//   const AddImage = () => {
+//     setImageList((prev) => [...prev, data]);
+//   }
 
-    let node = e.target;
-    const fileInput = document.querySelector('.file-input');
+//   useEffect(() => {
+//     setImageList([]);
+//   }, []);
 
-    fileInput.click();
-    fileInput.onchange = (event) => {
-      if (!fileExtentionCheck(event)) return;
+//   // 이미지 파일인지 확인
+//   const fileExtentionCheck = (event) => {
+//     const file = event.target;
+//     const fileName = file.files[0].name.toLowerCase();
 
-      if (node.nodeName === 'IMG') {
-        node = node.parentNode;
-      }
+//     if (fileName.endsWith('jpg') || fileName.endsWith('png') || fileName.endsWith('jpeg')){
+//       return true;
+//     }
 
-      imagePreview(node, event);
-    };
-  };
+//     alert('png, jpg, jpeg 파일만 등록 가능합니다.');
+//     file.value = '';
+//     return false;
+//   };
 
-  // 사진 제거
-  const imageRemove = (target) => {
-    target.innerHTML = '';
-  };
+//   // 사진 업로드
+//   const imageUpload = (e) => {
+//     if (!e || !e.target) {
+//       return;
+//     }
+  
+//     if (e.target.className.includes('drag')) {
+//       e.target.classList.remove('drag');
+//       return;
+//     }
+  
+//     let node = e.target;
+//     const fileInput = document.querySelector('.file-input');
+  
+//     fileInput.click();
+//     fileInput.onchange = (event) => {
+//       if (!event || !event.target) {
+//         return;
+//       }
+  
+//       if (!fileExtentionCheck(event)) return;
+  
+//       if (node.nodeName === 'IMG') {
+//         node = node.parentNode;
+//       }
+  
+//       imagePreview(node, event, data);
+//     };
+//   };  
 
-  // 사진 미리보기
-  const imagePreview = (target, event) => {
-    const reader = new FileReader();
+//   // 사진 제거
+//   const imageRemove = (target) => {
+//     target.innerHTML = '';
+//     setImageList((prevImageList) =>
+//     prevImageList.filter((img) => img !== target.firstChild.getAttribute('src'))
+//   );
+//   };
 
-    if (!event.target.files[0]) return;
+//   // 사진 미리보기
+//   const imagePreview = (target, event) => {
+//     const reader = new FileReader();
 
-    imageRemove(target);
+//     if (!event.target.files[0]) return;
 
-    reader.onload = function (event) {
-      const img = document.createElement('img');
-      img.className = 'img';
-      img.setAttribute('src', event.target.result);
-      target.appendChild(img);
-      target.classList.remove('empty');
-      dragEvent(img);
-      if (typeof onImagesSelected === 'function') {
-        onImagesSelected(event.target.result);
-      }
-    };
+//     imageRemove(target);
 
-    reader.readAsDataURL(event.target.files[0]);
-    const fileInput = document.querySelector('.file-input');
-    fileInput.value = '';
-  };
+//     reader.onload = function (event) {
+//       const img = document.createElement('img');
+//       img.className = 'img';
+//       img.setAttribute('src', event.target.result);
+//       target.appendChild(img);
+//       target.classList.remove('empty');
+//       dragEvent(img);
+//       if (typeof onImagesSelected === 'function') {
+//         onImagesSelected(event.target.result);
+//       }
 
-  // 이미지 드래그
-  const dragEvent = (target) => {
-    let isPress = false;
-    let prevPosX = 0;
-    let prevPosY = 0;
+//       // 이미지 목록 업데이트: 빈 요소에 대체하여 저장
+//       setImageList((prevImageList) => {
+//         const index = prevImageList.findIndex((item) => item === null);
+//         if (index !== -1) {
+//           const newImageList = [...prevImageList];
+//           newImageList[index] = event.target.result;
+//           return newImageList;
+//         }
+//           return prevImageList;
+//         });
+//     };
 
-    const dragStart = (e) => {
-      isPress = true;
-      prevPosX = e.clientX;
-      prevPosY = e.clientY;
-    };
+//     reader.readAsDataURL(event.target.files[0]);
+//     const fileInput = document.querySelector('.file-input');
+//     fileInput.value = '';
+//   };
 
-    const dragEnd = () => {
-      isPress = false;
-    };
+//   // 이미지 드래그
+//   const dragEvent = (target) => {
+//     let isPress = false;
+//     let prevPosX = 0;
+//     let prevPosY = 0;
 
-    const moveImage = (e) => {
-      if (!isPress) return;
+//     const dragStart = (e) => {
+//       isPress = true;
+//       prevPosX = e.clientX;
+//       prevPosY = e.clientY;
+//     };
 
-      target.classList.add('drag');
+//     const dragEnd = () => {
+//       isPress = false;
+//     };
 
-      const posX = prevPosX - e.clientX;
-      const posY = prevPosY - e.clientY;
+//     const moveImage = (e) => {
+//       if (!isPress) return;
 
-      prevPosX = e.clientX;
-      prevPosY = e.clientY;
+//       target.classList.add('drag');
 
-      target.style.left = target.offsetLeft - posX + 'px';
-      target.style.top = target.offsetTop - posY + 'px';
-    };
+//       const posX = prevPosX - e.clientX;
+//       const posY = prevPosY - e.clientY;
 
-    target.onmousedown = dragStart;
-    window.onmouseup = dragEnd;
-    window.onmousemove = moveImage;
-  };
+//       prevPosX = e.clientX;
+//       prevPosY = e.clientY;
 
-  useEffect(() => {
-    const $imageFrame = document.querySelector('.img-frame');
-    const $imageSelects = $imageFrame.querySelectorAll('.img-select');
-    const setImagePage = () => {
-      $imageSelects.forEach((item) => (item.onclick = imageUpload));
-    };
+//       target.style.left = target.offsetLeft - posX + 'px';
+//       target.style.top = target.offsetTop - posY + 'px';
+//     };
 
-    setImagePage();
-  });
+//     target.onmousedown = dragStart;
+//     window.onmouseup = dragEnd;
+//     window.onmousemove = moveImage;
+//   };
 
-  return (
-    <StyledImageCard>
-      <div className="image-page hidden">
-        <div className="img-frame">
-          <div className='img-row-container'>
-            <div className="img-select empty"></div>
-            <div className="img-select empty"></div>
-          </div>
-          <div className='img-row-container'>
-            <div className="img-select empty"></div>
-            <div className="img-select empty"></div>
-          </div>
-        </div>
-        <input type="file" className="file-input hidden" accept=".jpeg, .png, .jpg" />
-      </div>
-    </StyledImageCard>
-  );
-}
+//   useEffect(() => {
+//     const $imageFrame = document.querySelector('.img-frame');
+//     const $imageSelects = $imageFrame.querySelectorAll('.img-select');
+//     const setImagePage = () => {
+//       $imageSelects.forEach((item) => {
+//         item.addEventListener('click', () => {
+//           imageUpload();
+//           AddImage();
+//         });
+//       });
+//     };
+
+//     setImagePage();
+//     console.log('Image List:', imageList);
+//   });
+
+//   // Render two images per row
+//   const renderImagesPerRow = () => {
+//     const imageRows = [];
+
+//     for (let i = 0; i < imageList.length; i += 2) {
+//       const imageRow = (
+//         <div className="img-row-container" key={i}>
+//           <div className="img-select">
+//             <img className="img" src={imageList[i]} alt={`Image ${i}`} />
+//           </div>
+//           {i + 1 < imageList.length && (
+//             <div className="img-select">
+//               <img className="img" src={imageList[i + 1]} alt={`Image ${i + 1}`} />
+//             </div>
+//           )}
+//         </div>
+//       );
+//       imageRows.push(imageRow);
+//     }
+
+//     // Add empty image placeholders to fill the row if there are fewer than two images in the last row
+//     const emptyImagePlaceholders = 4 - (imageList.length % 2 || 2);
+
+//     for (let i = 0; i < emptyImagePlaceholders; i++) {
+//       imageRows.push(
+//         <div className="img-row-container" key={`empty-${i}`}>
+//           <div className="img-select empty"></div>
+//         </div>
+//       );
+//     }
+
+//     return imageRows;
+//   };
+
+//   return (
+//     <StyledImageCard>
+//       <div className="image-page hidden">
+//         <div className="img-frame">
+//           {/* <div className='img-row-container'>
+//             {imageList.map((imageUrl, index) => (
+//                 <div className="img-select" key={index}>
+//                   <img className="img" src={imageUrl} alt={`Image ${index}`} />
+//                 </div>
+//               ))} */}
+//               {/* 빈 이미지 선택칸을 이미지 목록의 개수에 따라 렌더링 */}
+//               {/* {Array.from({ length: 4 - imageList.length }, (_, index) => (
+//                 <div className="img-select empty" key={index}></div>
+//               ))}
+//           </div> */}
+//           <ImageRow>{renderImagesPerRow()}</ImageRow>
+//           <ImageRow>{renderImagesPerRow()}</ImageRow>
+//         </div>
+//         <input type="file" className="file-input hidden" accept=".jpeg, .png, .jpg" />
+//       </div>
+//     </StyledImageCard>
+//   );
+// }
--- src/pages/DecoratePage.jsx
+++ src/pages/DecoratePage.jsx
@@ -1,133 +1,132 @@
-import React, { useState } from "react"; // eslint-disable-line no-unused-vars
-import styled from 'styled-components';
-import { Link } from "react-router-dom";
-import FrameTool from "../components/DecoratePage/FrameTool";
-import PhotoTool from "../components/DecoratePage/PhotoTool";
-import DecorateCard from "../components/DecoratePage/DecorateCard";
+// import React, { useState } from "react"; // eslint-disable-line no-unused-vars
+// import styled from 'styled-components';
+// import { Link } from "react-router-dom";
+// import FrameTool from "../components/DecoratePage/FrameTool";
+// import PhotoTool from "../components/DecoratePage/PhotoTool";
+// import DecorateCard from "../components/DecoratePage/DecorateCard";
 
-const StyledDecorate = styled.div`
-    width: 390px;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #F7F7F7;
+// const StyledDecorate = styled.div`
+//     width: 390px;
+//     height: 100vh;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     background-color: #F7F7F7;
 
-    header {
-        width: 390px;
-        height: 73px;
-        padding: 16px;
-        display: flex;
-        justify-content: space-between;
-    }
+//     header {
+//         width: 390px;
+//         height: 73px;
+//         padding: 16px;
+//         display: flex;
+//         justify-content: space-between;
+//     }
 
-    .decorate-box {
-        width: 358px;
-        height: 48px;
-        border-radius: 8px;
-        background-color: #EEEEEE;
-        padding: 4px;
-        display: flex;
-        gap: 8px;
-    }
+//     .decorate-box {
+//         width: 358px;
+//         height: 48px;
+//         border-radius: 8px;
+//         background-color: #EEEEEE;
+//         padding: 4px;
+//         display: flex;
+//         gap: 8px;
+//     }
 
-    .frame {
-        width: 172px;
-        height: 40px;
-        border-radius: 8px;
-        // background-color: black;
-        color: white;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+//     .frame {
+//         width: 172px;
+//         height: 40px;
+//         border-radius: 8px;
+//         // background-color: black;
+//         color: white;
+//         display: flex;
+//         justify-content: center;
+//         align-items: center;
+//     }
 
-    .photo {
-        width: 172px;
-        height: 40px;
-        border-radius: 8px;
-        // background-color: black;
-        color: white;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+//     .photo {
+//         width: 172px;
+//         height: 40px;
+//         border-radius: 8px;
+//         // background-color: black;
+//         color: white;
+//         display: flex;
+//         justify-content: center;
+//         align-items: center;
+//     }
 
-    .btn-container{
-        width: 118px;
-        height: 18px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 8px;
-    }
+//     .btn-container{
+//         width: 118px;
+//         height: 18px;
+//         display: flex;
+//         justify-content: center;
+//         align-items: center;
+//         gap: 8px;
+//     }
 
-    .btn-container > p {
-        color: white;
-    }
+//     .btn-container > p {
+//         color: white;
+//     }
 
-    .selected {
-        background-color: black;
-        color: white; /* (Optional) Change text color when selected */
-      }
+//     .selected {
+//         background-color: black;
+//         color: white; /* (Optional) Change text color when selected */
+//       }
 
-    .tool-container {
-        display: flex;
-        justify-content: center;
-        // align-items: center;
-        width: 390px;
-        height: 250px;
-        overflow-x: scroll;
-    }
-`
+//     .tool-container {
+//         display: flex;
+//         // justify-content: center;
+//         width: 356px;
+//         height: 77px;
+//         // overflow-x: scroll;
+//     }
+// `
 
-const DecoratePage = () => {
-  const [selectedButton, setSelectedButton] = useState("frame");
-  const [selectedFrameColor, setSelectedFrameColor] = useState(null); // 선택한 프레임 색상을 상태로 관리합니다.
+// const DecoratePage = () => {
+//   const [selectedButton, setSelectedButton] = useState("frame");
+//   const [selectedFrameColor, setSelectedFrameColor] = useState(null); // 선택한 프레임 색상을 상태로 관리합니다.
 
-  const handleFrameSelect = (frame) => {
-    setSelectedFrameColor(frame.color); // 클릭한 프레임의 색상을 상태로 설정합니다.
-  };
+//   const handleFrameSelect = (frame) => {
+//     setSelectedFrameColor(frame.color); // 클릭한 프레임의 색상을 상태로 설정합니다.
+//   };
 
-  return (
-      <StyledDecorate>
-        <header>
-          <button>
-            <Link to="/make">
-              <img src='/assets/icons/arrow-back.svg' alt='back-space'/>
-            </Link>
-          </button>
-          <button>Done</button>
-        </header>
-        <div className='decorate-container'>
-          <div className='decorate-box'>
-            <button
-              className={`frame ${selectedButton === "frame" ? "selected" : ""}`}
-              onClick={() => setSelectedButton("frame")}
-            >
-              <div className='btn-container'>
-                <img src='/assets/icons/palette.svg' alt='palette'/>
-                <div>Frame Design</div>
-              </div>
-            </button>
-            <button
-              className={`photo ${selectedButton === "photo" ? "selected" : ""}`}
-              onClick={() => setSelectedButton("photo")}
-            >
-              <div className='btn-container'>
-                <img src='/assets/icons/ai-image.svg'alt='ai-img'/>
-                <div>Photo AI</div>
-              </div>
-            </button>
-          </div>
-          <DecorateCard frameColor={selectedFrameColor} />
-        </div>
-        <div className="tool-container">
-          {selectedButton === "frame" && <FrameTool onFrameSelect={handleFrameSelect} />}
-          {selectedButton === "photo" && <PhotoTool/>}
-        </div>
-      </StyledDecorate>
-    )
-  };
+//   return (
+//       <StyledDecorate>
+//         <header>
+//           <button>
+//             <Link to="/make">
+//               <img src='/assets/icons/arrow-back.svg' alt='back-space'/>
+//             </Link>
+//           </button>
+//           <button>Done</button>
+//         </header>
+//         <div className='decorate-container'>
+//           <div className='decorate-box'>
+//             <button
+//               className={`frame ${selectedButton === "frame" ? "selected" : ""}`}
+//               onClick={() => setSelectedButton("frame")}
+//             >
+//               <div className='btn-container'>
+//                 <img src='/assets/icons/palette.svg' alt='palette'/>
+//                 <div>Frame Design</div>
+//               </div>
+//             </button>
+//             <button
+//               className={`photo ${selectedButton === "photo" ? "selected" : ""}`}
+//               onClick={() => setSelectedButton("photo")}
+//             >
+//               <div className='btn-container'>
+//                 <img src='/assets/icons/ai-image.svg'alt='ai-img'/>
+//                 <div>Photo AI</div>
+//               </div>
+//             </button>
+//           </div>
+//           <DecorateCard frameColor={selectedFrameColor} />
+//         </div>
+//         <div className="tool-container">
+//           {selectedButton === "frame" && <FrameTool onFrameSelect={handleFrameSelect} />}
+//           {selectedButton === "photo" && <PhotoTool/>}
+//         </div>
+//       </StyledDecorate>
+//     )
+//   };
   
-  export default DecoratePage;
+//   export default DecoratePage;
